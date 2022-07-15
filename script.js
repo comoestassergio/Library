52b49726a8253be039e99421d7eb6675a4394338
sergio <dirtycrew666@gmail.com>
2022-07-15 14:43:35 +0500

    Add comments and structurise the code

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const pagesForm = document.querySelector(".pages-form")
 const isRead = document.querySelector(".checkbox")
 const mainSection = document.querySelector("#main__section")
 
+// Data structure
+
 const books = []
 
 const Book = {
@@ -20,6 +22,8 @@ const Book = {
     }
 }
 
+// Listening for cliks on Add Book button
+
 addBookBtn.addEventListener("click", function(){
     popUp.classList.add("visible")
     mainSection.classList.add("dimmed")
@@ -29,8 +33,7 @@ addBookBtn.addEventListener("click", function(){
     })
 })
 
-isRead.addEventListener("change", checkBoxValue())
-
+// Create a Book object and trigger book card creation
 
 submitBtn.addEventListener("click", function(){
     pushBook(createBook(), books)
@@ -40,6 +43,67 @@ submitBtn.addEventListener("click", function(){
     bookGridAddBook()
     clearInput()
 })
+
+// Create a book card
+
+
+function bookGridAddBook() {
+    const bookCard = document.createElement("div")
+    bookCard.classList.add("book-card")
+    const bookTitle = document.createElement("p")
+    const bookAuthor = document.createElement("p")
+    const bookPages = document.createElement("p")
+    const readStatus = document.createElement("button")
+    const closeBtn = document.createElement("button")
+
+    bookTitle.classList.add("card__title")
+    bookAuthor.classList.add("card__author")
+    bookPages.classList.add("card__pages")
+    readStatus.classList.add("card__read", "btn")
+    closeBtn.classList.add("book-card__close-btn")
+
+    const lastBook = books[books.length-1]
+
+    bookTitle.textContent = lastBook.title
+    bookAuthor.textContent = lastBook.author
+    bookPages.textContent = `${lastBook.pages} pages`
+    checkBoxValue()
+    readStatus.textContent = lastBook.isRead
+
+    bookCard.dataset.id = books.indexOf(lastBook)
+    closeBtn.dataset.id = books.indexOf(lastBook)
+    readStatus.dataset.id = books.indexOf(lastBook)
+
+
+    bookCard.appendChild(bookTitle)
+    bookCard.appendChild(bookAuthor)
+    bookCard.appendChild(bookPages)
+    bookCard.appendChild(readStatus)
+    bookCard.appendChild(closeBtn)
+    mainSection.appendChild(bookCard)
+
+    readStatus.addEventListener("click", function(){
+        const currentBook = books[readStatus.dataset.id]
+
+        if (currentBook.isRead === "Not finished"){
+            currentBook.isRead = "Finished"
+        } else {
+            currentBook.isRead = "Not finished"
+        }
+
+        readStatus.textContent = currentBook.isRead
+    })
+
+    closeBtn.addEventListener("click", function(){
+        if (closeBtn.dataset.id === bookCard.dataset.id){
+            bookCard.style.display = "none";
+            let deletedBook = books.splice(books[bookCard.dataset.id], 1)
+            console.log(books)
+        }
+    })
+}
+
+// Utilities
 
 function createBook(){
     const newBook = Object.create(Book)
@@ -54,57 +118,15 @@ function pushBook(book, arr){
     arr.push(book)
 }
 
-function checkBoxValue() {
-    isRead.value === "true" ? isRead.value = "Finished" :
-    isRead.value = "Not finished yet"
-}
-
-
-function bookGridAddBook() {
-    const bookCard = document.createElement("div")
-    bookCard.classList.add("book-card")
-    const bookTitle = document.createElement("p")
-    const bookAuthor = document.createElement("p")
-    const bookPages = document.createElement("p")
-    const readStatus = document.createElement("p")
-    const closeBtn = document.createElement("button")
-
-    bookTitle.classList.add("card__title")
-    bookAuthor.classList.add("card__author")
-    bookPages.classList.add("card__pages")
-    readStatus.classList.add("card__read")
-    closeBtn.classList.add("book-card__close-btn")
-
-    const lastBook = books[books.length-1]
-
-    bookTitle.textContent = lastBook.title
-    bookAuthor.textContent = lastBook.author
-    bookPages.textContent = `${lastBook.pages} pages`
-    readStatus.textContent = lastBook.isRead
-
-    bookCard.dataset.id = books.indexOf(lastBook)
-    closeBtn.dataset.id = books.indexOf(lastBook)
-
-
-    bookCard.appendChild(bookTitle)
-    bookCard.appendChild(bookAuthor)
-    bookCard.appendChild(bookPages)
-    bookCard.appendChild(readStatus)
-    bookCard.appendChild(closeBtn)
-    mainSection.appendChild(bookCard)
-
-    closeBtn.addEventListener("click", function(){
-        if (closeBtn.dataset.id === bookCard.dataset.id){
-            bookCard.style.display = "none";
-            let deletedBook = books.splice(books[bookCard.dataset.id], 1)
-            console.log(books)
-        }
-    })
-}
-
 function clearInput() {
     titleForm.value = null
     authorForm.value = null
     pagesForm.value = null
-    isRead.value = false
+}
+
+function checkBoxValue() {
+    isRead.checked ? isRead.value = "Finished" :
+    isRead.value = "Not finished"
+
+    console.log(isRead.value)
 }
